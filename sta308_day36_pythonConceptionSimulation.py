4affd7c33b62d759845999cec1b35f9ed3108708
Thomas Fisher <40180281+tjfisher19@users.noreply.github.com>
2021-11-16 13:40:16 -0500

    Update sta308_day36_pythonConceptionSimulation.py

--- sta308_day36_pythonConceptionSimulation.py
+++ sta308_day36_pythonConceptionSimulation.py
@@ -32,25 +32,26 @@ def conception_simulation(alpha=1, beta=1):
     ##  but returns list of size k elements
     ## We are only return 1, so we want to access preg[0]
     preg = rd.choices(("Pregnant", "Not"), weights=(prob_conception, 1-prob_conception),k=1)
+    pret = preg[0]
     mon = 1       ## takes at least one month
     
     ## Some status indicators
     consult = 0
     stop = 0
-    while(preg[0]=="Not" and stop==0):
+    while(preg=="Not" and stop==0):
         mon = mon + 1
         
         ## Aging effect
         prob_conception = prob_conception*0.99
-        preg = rd.choices(("Pregnant", "Not"), weights=(prob_conception, 1-prob_conception),k=1)
+        preg = rd.choices(("Pregnant", "Not"), weights=(prob_conception, 1-prob_conception),k=1)[0]
     
         ## Check the status
         ##   12 months and still not pregnant? Seek help
         ##   24 months and still not pregnant, sop the process
-        if(mon==12 and preg[0]=="Not"):  ## tried 12 months and not pregnant
+        if(mon==12 and preg=="Not"):  ## tried 12 months and not pregnant
             consult = 1
             prob_conception = 1.15*prob_conception
-        if(mon==24 and preg[0]=="Not"):
+        if(mon==24 and preg=="Not"):
             stop = 1
             mon = None
     return [mon, prob_conception, consult, stop]
@@ -63,37 +64,7 @@ for i in range(1000):
     mons.append(out[i][0])
 
 #%%
-
-import numpy as np
-
-np.mean(mons)
-
-## By default, numpy does not handle missing values well
-
-import pandas as pd
-
-mons_data = pd.Series(mons)
-
-np.mean(mons_data)
-np.nanmean(mons_data)
-np.sum(mons_data.isnull())
-
-np.std(mons_data)
-np.median(mons_data)
-np.nanmedian(mons_data)
-
-import matplotlib.pyplot as plt
-
-plt.hist(mons_data)
-
-
-
-
-preg = []
-x = 0
-for i in range(100000):
-    preg.append(rd.choices(("Pregnant", "Not"), weights=(0.3, 0.7), k=1))
-    if(preg[i][0]=="Not"):
-        x = x + 1
+## We will edit the below to do some light data analysis
+##
 
     
